617dfa2127b12450bab2fde72a428378b4761504
SUSHMANA GUPTA <sushmanag73@gmail.com>
2020-08-14 06:09:23 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,11 +3,11 @@ var playlist={
 };
 
 function updatePlaylist(playlist,artistName,songTitle){
-Object.assign({},playlist,{[artistName]:songTitle});
+playlist[artistName]=songTitle;
   return playlist;
 }
 
 function removeFromPlaylist(playlist,artistName){
-  delete playlist.artistName;
+  delete playlist[artistName];
   return playlist;
 }
